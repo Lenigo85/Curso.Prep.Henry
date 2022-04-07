522c7d97883edb3740decb2d26e87bcbb1e257ca
Lenigo85 <leandrogonzalez.1985@gmail.com>
2022-04-07 16:20:47 -0300

    JS-Extra-Homeworks completado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,14 +10,24 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  return Object.entries(objeto);
 }
 
 
 function numberOfCharacters(string) {
-  //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
+  //La función recibe un string. Recorre el string y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+    var caracteres = {};
+    for (var i = 0; i < string.length; i++) {
+      if (caracteres.hasOwnProperty(string[i])){
+        caracteres[string[i]] += 1;
+    } else {
+      caracteres[string[i]] = 1;
+    } 
+  } return caracteres;
+  console.log(caracteres);
 }
 
 
@@ -26,6 +36,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayuscula = "";
+  var minuscula = "";
+
+  for ( var i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      mayuscula = mayuscula + s[i];
+    } else {
+      minuscula = minuscula + s[i];
+    } 
+  } return (mayuscula + minuscula);
 }
 
 
@@ -35,7 +55,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var cambios = str.split(" ").map(function(el){
+    return el.split("").reverse().join("")
+  })
+  return cambios.join(" ");
+}
 
 
 function capicua(numero){
@@ -43,6 +67,12 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numeroInvertido = Number(numero.toString().split("").reverse().join(""));
+  if (numero === numeroInvertido) {
+    return "Es capicua";
+  } else {
+    return "No es capicua";
+  }
 }
 
 
@@ -50,6 +80,13 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var cadenaModificada = "";
+  for (var i = 0; i < cadena.length; i++) {
+    if (cadena[i] !== "a" & cadena[i] !== "b" & cadena[i] !== "c") {
+    //  cadenaModificada += cadena[i];
+    cadenaModificada = cadenaModificada + cadena[i];
+    } 
+  } return cadenaModificada;
 }
 
 
@@ -57,6 +94,16 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for (var i = 0; i < arr.length; i++){
+    for (var j = i + 1; j < arr.length; j++){
+      if (arr[i].length > arr[j].length) {
+        var temporal = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temporal;
+      }
+    }
+  }
+  return arr;
 }
 
 
@@ -65,8 +112,17 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
-}
+  //Escribe tu código aquí
+  var numerosEnComun = [];
+  for (var i = 0; i < arreglo1.length; i++) {
+    for (var j = 0; j < arreglo2.length; j++) {
+        if (arreglo2[j] === arreglo1[i]) {
+          numerosEnComun.push(arreglo2[j]);
+        }
+    }
+  }
+  return numerosEnComun;
+}  
 
 
 
